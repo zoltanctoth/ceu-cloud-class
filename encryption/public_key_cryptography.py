157ea1c8a429a377e35952c18df58d75c57c6ec6
Zoltan C. Toth <zoltan@zoltanctoth.com>
2023-11-07 10:33:46 +0000

    finished pki

--- encryption/public_key_cryptography.py
+++ encryption/public_key_cryptography.py
@@ -1,13 +1,17 @@
-import base64
-import os
+# %%
 from pathlib import Path
 
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-PROJECT_FOLDER = Path(__file__).parent[2]
-PRIVATE_KEY_FILE = Path(PROJECT_FOLDER) / "my_keypair"
-PUBLIC_KEY_FILE = Path(PROJECT_FOLDER) / "my_keypair.pub"
+PROJECT_FOLDER = Path(__file__).parent.parent
+PRIVATE_KEY_FILE = PROJECT_FOLDER / "my_keypair"
+PUBLIC_KEY_FILE = PROJECT_FOLDER / "my_keypair.pub"
+
+assert Path.exists(PRIVATE_KEY_FILE)
+assert Path.exists(PUBLIC_KEY_FILE)
+
+# %%
 
 # Load the private key from file
 with open(PRIVATE_KEY_FILE, "r") as key_file:
@@ -15,3 +19,22 @@ with open(PRIVATE_KEY_FILE, "r") as key_file:
 
 # Generate public key from the private key
 public_key = private_key.publickey()
+
+
+# %%
+short_secret_message = "My Secret Message".encode("utf-8")
+public_key_cipher = PKCS1_OAEP.new(public_key)
+encrypted_message = public_key_cipher.encrypt(short_secret_message)
+print(f"Encrypted message: {encrypted_message}")
+
+ENCRYPTED_MESSAGE_FILE = PROJECT_FOLDER / "encrypted_message.bin"
+with open(ENCRYPTED_MESSAGE_FILE, "wb") as f:
+    f.write(encrypted_message)
+
+# %%
+with open(ENCRYPTED_MESSAGE_FILE, "rb") as f:
+    encrypted_message_from_file = f.read()
+
+private_key_cipher = PKCS1_OAEP.new(private_key)
+decrypted_message = private_key_cipher.decrypt(encrypted_message_from_file)
+print(f"Decrypted message: {decrypted_message.decode('utf-8')}")
