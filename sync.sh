196d0fedf7285895bcccc9cdb63c0fd61fc5fab3
Zoltan C. Toth <zoltan@zoltanctoth.com>
2024-12-03 19:03:54 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ curl -s https://raw.githubusercontent.com/CEU-Economics-and-Business/ECBS-5147-D
 
 # Sync serverless folder
 rm -rf serverless
-curl -sL https://github.com/CEU-Economics-and-Business/ECBS-5147-Data-Engineering-2-Cloud-Computing/archive/main.zip | bsdtar -xf - --strip=1 "ECBS-5147-Data-Engineering-2-Cloud-Computing-main/serverless"
+curl -sL https://github.com/CEU-Economics-and-Business/ECBS-5147-Data-Engineering-2-Cloud-Computing/archive/main.zip | tar -xf - --strip=1 "ECBS-5147-Data-Engineering-2-Cloud-Computing-main/serverless"
 
 # Install requirements
 pip install -r requirements.txt
